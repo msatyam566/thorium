f7347d2613d0ade07fdff613bf4b241df496dacc
msatyam566 <msatyam566@gmail.com>
2022-03-04 22:59:06 +0530

    new

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,5 @@
-const UserModel= require("../models/userModel")
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+const Middleware = function(req, res){
+    res.send("all operation of middleware done")
+  }
+  
+  module.exports.Middleware=Middleware
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
+
+const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
+const middlewareMW = require("../middlewares/commonMiddleware.js")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+//router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+//router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+//router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+//router.get("/getBooksData", BookController.getBooksData)
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
+//router.post("/updateBooks", BookController.updateBooks)
+//router.post("/deleteBooks", BookController.deleteBooks)
 
 //MOMENT JS
 const moment = require('moment');
@@ -37,5 +39,10 @@ router.get("/dateManipulations", function (req, res) {
 
     res.send({ msg: "all good"})
 })
+router.get("/createUser", middlewareMW.mid1, middlewareMW.mid2, middlewareMW.mid3, UserController.Middleware);
+router.get("/basicAPi", middlewareMW.mid1, middlewareMW.mid2, middlewareMW.mid3, UserController.Middleware);
+router.get("/falanaAPI", middlewareMW.mid1, middlewareMW.mid2, middlewareMW.mid3, UserController.Middleware);
+
+module.exports = router;
 
 module.exports = router;
